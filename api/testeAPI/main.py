e1bb16741d4171916505068ea95bb50937bb418e
Paulo Telles <ptelles020@gmail.com>
2023-11-14 10:30:38 -0300

    alteração de estrutura/ mudei tudo para pasta de templates e static

--- api/testeAPI/main.py
+++ api/testeAPI/main.py
@@ -1,13 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 from establishment_blueprint import establishment_blueprint
-from database import Session
 
 app = Flask(__name__, template_folder="templates", static_folder="static")
 app.register_blueprint(establishment_blueprint, url_prefix='/establishment')
 
 @app.route("/")
 def home():
-    return "Landing Page"
+    return render_template('landingPage.html')
+
+@app.route("/servicos")
+def servicos():
+    return render_template("servicos.html")
+
+@app.route("/contato")
+def contato():
+    return render_template("contato.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
