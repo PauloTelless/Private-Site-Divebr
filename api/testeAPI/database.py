02d2f77c647b2e5fe02c86124f711577b9f7fe64
americo.feitoza@gmail.com <americo.feitoza@gmail.com>
2023-11-15 00:39:00 -0300

    mudei a fonte em serviços e Dive da landing page.

--- api/testeAPI/database.py
+++ api/testeAPI/database.py
@@ -5,7 +5,9 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base() #Cria uma base modelo (Fazer classes padrões)
 # Configuração do SQLAlchemy
-engine = create_engine('mysql+pymysql://root:@localhost:3306/establishment', echo=True) #Instância criada para fazer as configurações do SQLAlchemy
+senha = "Divesoosloucossabem!123"
+
+engine = create_engine(f'mysql+pymysql://root:{senha}@localhost:3306/establishment', echo=True) #Instância criada para fazer as configurações do SQLAlchemy
 Base.metadata.create_all(engine)  # Cria todas a tabelas definidas em database.py
 Session = sessionmaker(bind=engine) #Cria uma sessão de objetos
 
